4626e50874d899a344e3b745d08145d4f50633d5
mori-tt <morit0301@gmail.com>
2023-11-22 01:26:09 +0900

    ♻️ ヘッドライン部分をコンポーネント化

--- pages/about.js
+++ pages/about.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import { Headline } from '../components/Headline.js'
 import { Links } from '../components/Links.js'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,6 +17,21 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
+        <div className={styles.description}>
+          <Headline title="About Page"/>
+        </div>
+
+        <div className={styles.center}>
+          <Image
+            className={styles.logo}
+            src="/next.svg"
+            alt="Next.js Logo"
+            width={180}
+            height={37}
+            priority
+          />
+        </div>
+
         <Links />
       </main>
     </>
