8e037d439aa9fdbc560a9ff5a63420442cc19a4f
mori-tt <morit0301@gmail.com>
2023-11-27 22:35:17 +0900

    ♻️ Linksのリファクタリング

--- components/Links.js
+++ components/Links.js
@@ -1,59 +1,54 @@
 import classes from "./Links.module.css";
 
+const ITEMS = [
+  {
+    href: "https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    title: "Docs",
+    desctiption:
+      "Find in-depth information about Next.js features and API.",
+  },
+  {
+    href: "https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    title: "Learn",
+    desctiption:
+      "Learn about Next.js in an interactive course with quizzes!",
+  },
+  {
+    href: "https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    title: "Templates",
+    desctiption:
+      "Find in-depth information about Next.js features and API.",
+  },
+  {
+    href: "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    title: "Deploy",
+    desctiption:
+      "Instantly deploy your Next.js site to a shareable URL with Vercel.",
+  },
+];
+
 export function Links() {
   return (
     <>
       <div className={classes.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className={classes.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={classes.title}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={classes.desctiption}>Find in-depth information about Next.js features and&nbsp;API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className={classes.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={classes.title}>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p className={classes.desctiption}>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className={classes.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={classes.title}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={classes.desctiption}>Discover and deploy boilerplate example Next.js&nbsp;projects.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className={classes.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={classes.title}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={classes.desctiption}>
-            Instantly deploy your Next.js site to a shareable URL
-            with&nbsp;Vercel.
-          </p>
-        </a>
+        {ITEMS.map((item) =>{
+          return (
+            <a
+              key={item.href}
+              href={item.href}
+              className={classes.card}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h2 className={classes.title}>
+                {item.title}
+                <span> -&gt;</span>
+              </h2>
+              <p className={classes.desctiption}>{item.desctiption}</p>
+            </a>
+          );
+        })}
+       
       </div>
     </>
   );
