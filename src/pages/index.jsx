9d2e82710bb0f5a578272c176237764ba716eae6
mori-tt <morit0301@gmail.com>
2023-12-01 02:41:30 +0900

    🚧 クリックイベントの実験

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,16 +2,29 @@ import Head from "next/head";
 import { Inter } from "next/font/google";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
+import { useCallback } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+
+  const foo = 1;
+
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  }, []);
+
   return (
     <>
       <Head>
         <title>Create Next App</title>
       </Head>
       <Header />
+      <a href="/about" onClick={handleClick}>
+        ボタン
+      </a>
       <Main page="index" />
     </>
   );
