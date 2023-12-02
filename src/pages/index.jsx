40501d778f07ed3f8fde58af55886b8da72db463
mori-tt <morit0301@gmail.com>
2023-12-03 03:38:54 +0900

    ✨ カウンターを設置してuseStateでカウントアップ

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,29 +2,31 @@ import Head from "next/head";
 import { Inter } from "next/font/google";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useCallback, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
 
-  const foo = 1;
+  const [count, setCount] = useState(1);
 
-  const handleClick = useCallback((e) => {
-    console.log(e.target.href);
-    e.preventDefault();
-    alert(foo);
-  }, []);
+  // let foo = 1;
+
+  const handleClick = (e) => {
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
+    // foo = foo + 1;
+  };
 
   useEffect(()=>{
-    console.log("マウント時");
     document.body.style.backgroundColor = "lightblue"
 
     return () => {
-      console.log("アンマウント時");
       document.body.style.backgroundColor = "";
     }
   }, []);
+
+  // console.log(count);
 
   return (
     <>
@@ -32,9 +34,10 @@ export default function Home() {
         <title>Create Next App</title>
       </Head>
       <Header />
-      <a href="/about" onClick={handleClick}>
+      <h1>{count}</h1>
+      <button href="/about" onClick={handleClick}>
         ボタン
-      </a>
+      </button>
       <Main page="index" />
     </>
   );
