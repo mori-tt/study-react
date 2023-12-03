e5ed7efcead1d8c1cde297820f461de72fcc8750
mori-tt <morit0301@gmail.com>
2023-12-03 22:55:26 +0900

    ✨ useStateでstringやbooleanを扱う

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,21 +7,34 @@ import { useCallback, useEffect, useState } from "react";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-
   const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
 
   const handleClick = useCallback(() => {
     if (count < 10) {
-    setCount((count) => count + 1);
+      setCount((count) => count + 1);
     }
   }, [count]);
 
-  useEffect(()=>{
-    document.body.style.backgroundColor = "lightblue"
+  const handleDisplay = useCallback(() => {
+    setIsShow((isShow) => !isShow);
+  }, []);
+
+  const handleChange = useCallback((e) => {
+    if (e.target.value.length > 5) {
+      alert("5文字以内にしてください");
+      return;
+    }
+    setText(e.target.value.trim());
+  }, []);
+
+  useEffect(() => {
+    document.body.style.backgroundColor = "lightblue";
     return () => {
       document.body.style.backgroundColor = "";
-    }
-  }, [count]);
+    };
+  }, []);
 
   return (
     <>
@@ -29,10 +42,14 @@ export default function Home() {
         <title>Create Next App</title>
       </Head>
       <Header />
-      <h1>{count}</h1>
+      {isShow ? <h1>{count}</h1> : null}
       <button href="/about" onClick={handleClick}>
         ボタン
       </button>
+      <button href="/about" onClick={handleDisplay}>
+        {isShow ? "非表示" : "表示"}
+      </button>
+      <input type="text" value={text} onChange={handleChange} />
       <Main page="index" />
     </>
   );
