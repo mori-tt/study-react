9844ce9b0fe379ae1306dec26080ff3450b10318
mori-tt <morit0301@gmail.com>
2023-12-04 21:02:43 +0900

    🎨 Custom Hooksを使って分割化

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,15 +3,41 @@ import { Inter } from "next/font/google";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
 
+import { useCounter } from "@/src/hooks/useCounter";
+import { useInputArray } from "@/src/hooks/useInputArray";
+import { useBgLigntBlue } from "@/src/hooks/useBgLigntBlue";
+
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function About() {
+  const { count, isShow, handleClick, handleDisplay } = useCounter();
+  const { text, array, handleChange, handleAdd } = useInputArray();
+
+  useBgLigntBlue();
+
   return (
     <>
       <Head>
         <title>Create Next App</title>
       </Head>
       <Header />
+
+      {isShow ? <h1>{count}</h1> : null}
+      <button href="/about" onClick={handleClick}>
+        ボタン
+      </button>
+      <button href="/about" onClick={handleDisplay}>
+        {isShow ? "非表示" : "表示"}
+      </button>
+
+      <input type="text" value={text} onChange={handleChange} />
+      <button onClick={handleAdd}>追加</button>
+      <ul>
+        {array.map((item) => {
+          return <li key={item}>{item}</li>;
+        })}
+      </ul>
+
       <Main page="about" />
     </>
   );
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,50 +2,18 @@ import Head from "next/head";
 import { Inter } from "next/font/google";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useCallback, useEffect, useState } from "react";
+
+import { useCounter } from "@/src/hooks/useCounter";
+import { useInputArray } from "@/src/hooks/useInputArray";
+import { useBgLigntBlue } from "@/src/hooks/useBgLigntBlue";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const [count, setCount] = useState(1);
-  const [text, setText] = useState("");
-  const [isShow, setIsShow] = useState(true);
-  const [array, setArray] = useState([]);
+  const { count, isShow, handleClick, handleDisplay } = useCounter();
+  const { text, array, handleChange, handleAdd } = useInputArray();
 
-  const handleClick = useCallback(() => {
-    if (count < 10) {
-      setCount((prevCount) => prevCount + 1);
-    }
-  }, [count]);
-
-  const handleDisplay = useCallback(() => {
-    setIsShow((prevIsShow) => !prevIsShow);
-  }, []);
-
-  const handleChange = useCallback((e) => {
-    if (e.target.value.length > 5) {
-      alert("5文字以内にしてください");
-      return prevArray;
-    }
-    setText(e.target.value.trim());
-  }, []);
-
-  const handleAdd = useCallback(() => {
-    setArray((prevArray) => {
-      if (prevArray.some((item) => item === text)) {
-        alert("同じ要素が既に存在します。");
-        return prevArray;
-      }
-      return [...prevArray, text];
-    });
-  }, [text]);
-
-  useEffect(() => {
-    document.body.style.backgroundColor = "lightblue";
-    return () => {
-      document.body.style.backgroundColor = "";
-    };
-  }, []);
+  useBgLigntBlue();
 
   return (
     <>
@@ -53,6 +21,7 @@ export default function Home() {
         <title>Create Next App</title>
       </Head>
       <Header />
+
       {isShow ? <h1>{count}</h1> : null}
       <button href="/about" onClick={handleClick}>
         ボタン
@@ -60,6 +29,7 @@ export default function Home() {
       <button href="/about" onClick={handleDisplay}>
         {isShow ? "非表示" : "表示"}
       </button>
+
       <input type="text" value={text} onChange={handleChange} />
       <button onClick={handleAdd}>追加</button>
       <ul>
@@ -67,6 +37,7 @@ export default function Home() {
           return <li key={item}>{item}</li>;
         })}
       </ul>
+
       <Main page="index" />
     </>
   );
