0c8a8dc14aefd6dccb2b76da311fd33eb3da68cd
mori-tt <morit0301@gmail.com>
2023-12-06 01:07:19 +0900

    ✨ useRouterを使用

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,12 +2,12 @@ import "src/styles/globals.css";
 import Head from "next/head";
 import { useCounter } from "@/src/hooks/useCounter";
 import { useInputArray } from "@/src/hooks/useInputArray";
-import { useBgLigntBlue } from "@/src/hooks/useBgLigntBlue";
+import { useBgColor } from "@/src/hooks/useBgColor";
 
 export default function App({ Component, pageProps }) {
   const counter = useCounter();
   const inputArray = useInputArray();
-  useBgLigntBlue();
+  useBgColor();
 
   return (
     <>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,10 +3,6 @@ import { Inter } from "next/font/google";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
 
-import { useCounter } from "@/src/hooks/useCounter";
-import { useInputArray } from "@/src/hooks/useInputArray";
-import { useBgLigntBlue } from "@/src/hooks/useBgLigntBlue";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home(props) {
